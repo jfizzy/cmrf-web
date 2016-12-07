50e51af419b7e881635a335627c3aa6ea5f66976
james.macisaac <james.macisaac@zone18-wb.cs.ucalgary.ca>
2016-12-06 17:55:23 -0700

    Changes some styles for the tables and the links on the tables

--- app/main/errors.py
+++ app/main/errors.py
@@ -9,7 +9,6 @@ def forbidden(e):
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
