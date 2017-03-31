8fd5ce5693c3b4387197a46a70c61d8cd3a58132
James MacIsaac <james@jmproj.com>
2017-03-30 20:31:51 -0600

    Updated manage script to reflect large changes to model. Made a database migration.

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if os.environ.get('FLASK_COVERAGE'):
     COV.start()
 
 from app import create_app, db
-from app.models import User, Role, FundingAccount, WorkOrder, Report, LogEntry
+from app.models import User, Role, WorkOrder, Report
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -17,9 +17,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role,
-                FundingAccount=FundingAccount, WorkOrder=WorkOrder, Report=Report,
-                LogEntry=LogEntry)
+    return dict(app=app, db=db, User=User, Role=Role, WorkOrder=WorkOrder, Report=Report)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
