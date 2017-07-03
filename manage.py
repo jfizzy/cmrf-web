61fb62f52d89ab9422fb44a7374141a5ecdbc78e
James MacIsaac <james@jmproj.com>
2017-07-03 16:49:18 -0600

    New db migrations created, ignoring file uploads folder

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ if os.environ.get('FLASK_COVERAGE'):
     COV.start()
 
 from app import create_app, db
-from app.models import User, Role, WorkOrder, Report, NewsItem
+from app.models import User, Role, WorkOrder, Report, NewsItem, Publication
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -17,7 +17,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, WorkOrder=WorkOrder, Report=Report)
+    return dict(app=app, db=db, User=User, Role=Role, WorkOrder=WorkOrder, Report=Report, NewsItem=NewsItem, Publication=Publication)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
